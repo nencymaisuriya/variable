5a38c37104babff6bf522f1661a93ff315544f17
nencymaisuriya <160009308+nencymaisuriya@users.noreply.github.com>
2024-11-20 18:48:53 +0530

    Add files via upload

---
+++ datatypevariable.js
@@ -0,0 +1,25 @@
+// what is a variable= like container store to information or data
+
+//initailazation
+// var a;
+// let b;
+// const PI=3.14;
+
+// datatypes two types=>1 primitive,2 non primitives
+//1 primitives
+            // number,string,boolean,null, symbol,undefine,bigint
+//2 non primitives
+            // object
+
+let num=20;
+let str="21";
+let boolean=true;
+let a;
+var Null;
+console.log(num);
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof boolean );
+console.log(typeof a);
+console.log(typeof null);
+
